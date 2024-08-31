f51fefbd79a9fd1db0b2fb777538799f1f4f1181
sagarb2003 <sagarssb2003@gmail.com>
2024-09-01 00:56:25 +0530

    create the form for input  and created the floors and lifts

---
+++ src/js/main.js
@@ -0,0 +1,108 @@
+const form = document.querySelector("form");
+const displayAllFloors = document.getElementById("displayAllFloors");
+
+let floors=[]
+let liftsInfo = [];
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const lifts = Number(document.getElementById("no-of-lifts").value);
+  const floors = Number(document.getElementById("no-of-floors").value);
+
+  if ( !lifts || !floors || (lifts <=0 || floors <=0)) {
+    alert("Enter correct number of lifts and floors");
+    return; 
+  }
+  displayFloorsAndLifts(lifts,floors);
+  form.style.display = "none";
+});
+
+const displayFloorsAndLifts=(liftsCount,floorsCount)=>{
+    displayFloors(floorsCount,liftsCount);
+    displayLifts(liftsCount);
+}
+
+const displayFloors=(floorsCount,liftsCount)=>{
+    const viewportWidth = window.innerWidth;
+    // console.log(viewportWidth);
+    const requiredWidth = 70 * liftsCount + 80;
+    for(let i=0;i<floorsCount;i++){
+        //CREATING FLOORS
+        const floor=document.createElement('div');
+        floor.classList.add('floor')
+        floor.id=`floor${floorsCount-i-1}`
+        floor.style.width =
+          viewportWidth > requiredWidth
+            ? `${viewportWidth}px`
+            : `${requiredWidth}px`;
+
+        //UP BUTTON
+        const upButton=document.createElement('button');
+        upButton.innerText='Up';
+        upButton.id = `up${floorsCount - i - 1}`;
+        upButton.classList.add('up');
+        // upButton.addEventListener('click',buttonClickHandler);
+
+        //DOWN BUTTON
+        const downButton=document.createElement('button');
+        downButton.innerText = "Down";
+        downButton.id = `down${floorsCount - i - 1}`;
+        downButton.classList.add("down");
+        // downButton.addEventListener("click", buttonClickHandler);
+
+        //CREATING SPAN OF FLOOR NUMBERS
+        const floorNumber=document.createElement('span');
+        floorNumber.classList.add("floor-number");
+        floorNumber.innerText = `Floor ${floorsCount - i - 1}`;
+        const buttonsContainer = document.createElement("div");
+        buttonsContainer.classList.add("buttons-box");
+        if (i > 0) buttonsContainer.appendChild(upButton);
+        buttonsContainer.appendChild(floorNumber);
+        if (i < floorsCount - 1) buttonsContainer.appendChild(downButton);
+
+        floor.appendChild(buttonsContainer);
+
+        displayAllFloors.appendChild(floor);
+
+        floors.push(floor);
+        
+    }
+}
+
+const displayLifts = (liftCount) => {
+  for (let i = 0; i < liftCount; i++) {
+    const floor0 = document.querySelector("#floor0");
+    const lift = document.createElement("div");
+    const leftDoor = document.createElement("div");
+    const rightDoor = document.createElement("div");
+
+    leftDoor.classList.add("door");
+    rightDoor.classList.add("door");
+    leftDoor.classList.add("left-door");
+    rightDoor.classList.add("right-door");
+
+    leftDoor.id = `left-door${i}`;
+    rightDoor.id = `right-door${i}`;
+
+    lift.appendChild(leftDoor);
+    lift.appendChild(rightDoor);
+    lift.classList.add("lift");
+
+    lift.id = `lift${i}`;
+    lift.style.left = `${100 + i * 100}px`;
+    const currLiftState = {
+      id: i,
+      currentFloor: 0,
+      domElement: lift,
+      innerHtML: ``,
+      isMoving: false,
+      lastButtonCalled: null,
+      isBusy: false,
+      goingTo: null,
+    };
+    floor0.appendChild(lift);
+    liftsInfo.push(currLiftState);
+  }
+
+};
+
